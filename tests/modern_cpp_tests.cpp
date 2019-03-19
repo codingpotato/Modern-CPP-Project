57961a33e1f03e778c35116aa79fe5cd4734f1b8
Codingpotato <codingpotato@gmail.com>
2019-03-19 11:03:25 +0800

    variant visit

--- tests/modern_cpp_tests.cpp
+++ tests/modern_cpp_tests.cpp
@@ -3,7 +3,27 @@
 
 #include "variant"
 
-TEST_CASE("variant", "[variant]") {
+template <class... Ts>
+struct overload : Ts... {
+  overload(Ts...) {}
+  using Ts::operator()...;
+};
+// template <class... Ts>
+// overload(Ts...)->overload<Ts...>;
+
+TEST_CASE("variant get", "[variant]") {
   std::variant<int, std::string> n{1};
   REQUIRE(std::get<int>(n) == 1);
+}
+
+TEST_CASE("variant visit", "[variant]") {
+  std::variant<int, std::string> n{100};
+  std::visit(overload{[](int value) { REQUIRE(value == 100); },
+                      [](const std::string &) { REQUIRE(false); }},
+             n);
+  std::variant<int, std::string> str{"string"};
+  std::visit(
+      overload{[](int) { REQUIRE(false); },
+               [](const std::string &value) { REQUIRE(value == "string"); }},
+      n);
 }
